6dab456a05388110f17a1e68b6925bca3c80962e
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-07-09 20:58:28 +0000

    Updates

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,69 +1,44 @@
-exec { 'apt-update':
-  command => '/usr/bin/apt-get -y update',
-  path    => '/usr/bin',
-  before  => Package['nginx'],
-}
-
+# Install Nginx
 package { 'nginx':
   ensure => installed,
 }
 
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => template('nginx/default.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
-}
-
-file { '/data/web_static/':
+# Create necessary directories
+file { '/data':
   ensure => directory,
 }
 
-file { '/data/web_static/releases/test/':
+file { '/data/web_static':
   ensure => directory,
 }
 
-file { '/data/web_static/shared/':
+file { '/data/web_static/releases':
   ensure => directory,
 }
 
+file { '/data/web_static/shared':
+  ensure => directory,
+}
+
+file { '/data/web_static/releases/test':
+  ensure => directory,
+}
+
+# Create a sample HTML file
 file { '/data/web_static/releases/test/index.html':
-  ensure  => file,
-  content => 'Hello Holberton School!',
+  ensure  => present,
+  content => '<html>\n<head>\n</head>\n<body>\n  Holberton School\n</body>\n</html>\n',
 }
 
-file { '/var/www/html/custom_404.html':
-  ensure  => file,
-  content => "Ceci n'est pas une page\n",
-}
-
+# Create a symbolic link
 file { '/data/web_static/current':
-  ensure => link,
+  ensure => 'link',
   target => '/data/web_static/releases/test',
 }
 
-exec { 'chown-ubuntu':
-  command => 'chown -R ubuntu:ubuntu /data/',
-  path    => '/usr/bin',
-}
-
-file_line { 'nginx-location':
-  path    => '/etc/nginx/sites-available/default',
-  line    => '  location /hbnb_static {',
-  match   => '  location / {',
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-exec { 'nginx-restart':
-  command     => 'service nginx restart',
-  path        => '/usr/bin',
-  refreshonly => true,
-  subscribe   => File_line['nginx-location'],
+# Set ownership to the ubuntu user and group
+file { '/data':
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  recurse => true,
 }

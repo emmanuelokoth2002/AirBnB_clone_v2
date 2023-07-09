f1064e0534acde579002dda2d72b02407ce66484
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-07-09 11:16:30 -0700

    Updates

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This module contains a Fabric function to create a .tgz archive from the contents of the web_static folder.
+This module contains a Fabric function to create a .tgz
+archive from the contents of the web_static folder.
 """
 
 from fabric.api import local
@@ -12,7 +13,8 @@ def do_pack():
     Create a .tgz archive from the contents of the web_static folder.
 
     Returns:
-        str: The path of the created archive file, or None if the archive creation failed.
+        str: The path of the created archive file, or
+        None if the archive creation failed.
     """
     now = datetime.now()
     timestamp = now.strftime("%Y%m%d%H%M%S")

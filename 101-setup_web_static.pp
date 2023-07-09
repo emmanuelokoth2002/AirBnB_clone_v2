15e6f0680dc70dff3d3906dbe154cb70b7197281
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-07-09 20:54:35 +0000

    Updates

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,40 +1,69 @@
-# Install Nginx
+exec { 'apt-update':
+  command => '/usr/bin/apt-get -y update',
+  path    => '/usr/bin',
+  before  => Package['nginx'],
+}
+
 package { 'nginx':
   ensure => installed,
 }
 
-# Create necessary directories
-file { '/data/web_static':
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => template('nginx/default.erb'),
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/etc/nginx/sites-available/default'],
+}
+
+file { '/data/web_static/':
   ensure => directory,
 }
 
-file { '/data/web_static/releases':
+file { '/data/web_static/releases/test/':
   ensure => directory,
 }
 
-file { '/data/web_static/shared':
+file { '/data/web_static/shared/':
   ensure => directory,
 }
 
-file { '/data/web_static/releases/test':
-  ensure => directory,
-}
-
-# Create a sample HTML file
 file { '/data/web_static/releases/test/index.html':
-  ensure  => present,
-  content => '<html>\n<head>\n</head>\n<body>\n  Holberton School\n</body>\n</html>\n',
+  ensure  => file,
+  content => 'Hello Holberton School!',
 }
 
-# Create a symbolic link
+file { '/var/www/html/custom_404.html':
+  ensure  => file,
+  content => "Ceci n'est pas une page\n",
+}
+
 file { '/data/web_static/current':
-  ensure => 'link',
+  ensure => link,
   target => '/data/web_static/releases/test',
 }
 
-# Set ownership to the ubuntu user and group
-file { '/data':
-  owner => 'ubuntu',
-  group => 'ubuntu',
-  recurse => true,
+exec { 'chown-ubuntu':
+  command => 'chown -R ubuntu:ubuntu /data/',
+  path    => '/usr/bin',
+}
+
+file_line { 'nginx-location':
+  path    => '/etc/nginx/sites-available/default',
+  line    => '  location /hbnb_static {',
+  match   => '  location / {',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+exec { 'nginx-restart':
+  command     => 'service nginx restart',
+  path        => '/usr/bin',
+  refreshonly => true,
+  subscribe   => File_line['nginx-location'],
 }

44aaf2cd294e296457e4efd070916c9dce55e8e1
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-07-23 08:29:13 -0700

    Updates

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application to display an HTML page with Airbnb filters.
+"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """
+    Closes the database session after each request.
+    """
+    storage.close()
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    """
+    Displays an HTML page with Airbnb filters.
+    """
+    states = sorted(storage.all('State').values(), key=lambda state: state.name)
+    cities = sorted(storage.all('City').values(), key=lambda city: city.name)
+    amenities = sorted(storage.all('Amenity').values(), key=lambda amenity: amenity.name)
+    return render_template('10-hbnb_filters.html', states=states, cities=cities, amenities=amenities)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+

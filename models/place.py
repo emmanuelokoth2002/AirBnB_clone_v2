bdfec3b3ee2b27043576efd71c3846dc4d98ad7c
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-07-24 01:28:07 -0700

    Updates

--- models/place.py
+++ models/place.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+import models
 from os import getenv
 from models.base_model import Base, BaseModel
+from models.amenity import Amenity
 from sqlalchemy import Column, Float, ForeignKey, Integer, String, Table
 from sqlalchemy.orm import relationship
+from models.review import Review
 
 association_table = Table("place_amenity", Base.metadata,
                           Column("place_id", String(60),
@@ -27,7 +30,31 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0)
     latitude = Column(Float)
     longitude = Column(Float)
-
-    reviews = relationship("Review", backref="place", cascade="all, delete")
+    reviews = relationship("Review", backref="place", cascade="delete")
     amenities = relationship("Amenity", secondary="place_amenity",
                              viewonly=False, overlaps="place_amenities")
+    amenity_ids = []
+
+    if getenv("HBNB_TYPE_STORAGE", None) != "db":
+        @property
+        def reviews(self):
+            """Get a list of all linked Reviews."""
+            listReview = []
+            for review in list(models.storage.all(Review).values()):
+                if review.place_id == self.id:
+                    listReview.append(review)
+            return listReview
+
+        @property
+        def amenities(self):
+            """Get/set linked Amenities."""
+            listAmenity = []
+            for amenity in list(models.storage.all(Amenity).values()):
+                if amenity.id in self.amenity_ids:
+                    listAmenity.append(amenity)
+            return listAmenity
+
+        @amenities.setter
+        def amenities(self, value):
+            if type(value) == Amenity:
+                self.amenity_ids.append(value.id)
